1646cb178cc002e43de3b629c9540f701a8c78af
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2024-09-20 13:21:58 +0300

    Created db model for Books

--- React+Django-API/server/api/models.py
+++ React+Django-API/server/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    bookTitle = models.CharField(max_length=100)
+    releaseDate = models.DateField()
+
+    def __str__(self):
+        return self.bookTitle
